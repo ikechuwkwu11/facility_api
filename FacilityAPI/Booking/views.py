130af62237b43316dc1644a4bdfdaa9e64cedfcc
ikechuwkwu11 <mbaikechukwu10@gmail.com>
2025-04-21 08:51:02 -0700

    Add files via upload

--- FacilityAPI/Booking/views.py
+++ FacilityAPI/Booking/views.py
@@ -1,10 +1,5 @@
-from alembic.util import status
-from django.core.serializers import serialize
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from sqlalchemy.sql.coercions import expect
-
 from .serializer import FacilitySerializer,RegistrationSerializer,LoginSerializer,BookingSerializer
 from .models import Facility,Booking
 from django.contrib.auth import logout
@@ -53,7 +48,7 @@ class FacilitySingle(APIView):
             serializer = FacilitySerializer(facility)
             return Response(serializer.data)
         except Facility.DoesNotExist:
-            return Response({'error':'Internal server error'},status=400)
+            return Response({'error':'Does not exist'},status=400)
         except Exception as e:
             return Response({'error': 'Internal server error', 'details': str(e)},status=500)
 
@@ -77,6 +72,8 @@ class EditFacility(APIView):
                 serializer.save()
                 return Response({'message':'You have successfully edited the facility','facility':serializer.data},status=200)
             return Response({'message':'This has not been edited'},status=400)
+        except Facility.DoesNotExist:
+            return Response({'error': 'Does not exist'}, status=400)
         except Exception as e:
             return Response({'message':'Internal server error','error':str(e)},status=500)
 
@@ -86,6 +83,8 @@ class DeleteFacility(APIView):
             facility = Facility.objects.get(id=facility_id)
             facility.delete()
             return Response({'message':'facility has been deleted'},status=200)
+        except Facility.DoesNotExist:
+            return Response({'error': 'Does not exist'}, status=400)
         except Exception as e:
             return Response({'message':'internal server error','error':str(e)},status=500)
 
@@ -115,6 +114,8 @@ class BookingSingle(APIView):
             booking = Booking.objects.get(id= booking_id)
             serializer = BookingSerializer(booking)
             return Response(serializer.data, status=200)
+        except Booking.DoesNotExist:
+            return Response({'error': 'Does not exist'}, status=400)
         except Exception as e:
             return Response({'message':'internal server error', 'error':str(e)}, status=500)
 
@@ -127,6 +128,8 @@ class EditBooking(APIView):
                 serializer.save()
                 return Response({'message':'This booking has been edited'},status=200)
             return Response({'message':'This booking has not been edited'},status=400)
+        except Booking.DoesNotExist:
+            return Response({'error': 'Does not exist'}, status=400)
         except Exception as e:
             return Response({'message':"internal server error",'error':str(e)},status=500)
 
@@ -136,5 +139,7 @@ class DeleteBooking(APIView):
             booking = Booking.objects.get(id = booking_id)
             booking.delete()
             return Response({'message':'This booking has been deleted'},status=200)
+        except Booking.DoesNotExist:
+            return Response({'error': 'Does not exist'}, status=400)
         except Exception as e:
             return Response({'message':'internal server error','error':str(e)},status=500)
